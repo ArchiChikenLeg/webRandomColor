1b79822ff3ae80e12581ee460f45d366df77fae5
Lidiia <andrienkolidia@gmail.com>
2023-08-10 17:40:13 +0300

    copying text added

--- script.js
+++ script.js
@@ -32,12 +32,17 @@ function setTextColor(text, color){
     const luminance = chroma(color).luminance();
     text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
+function copyText(text){
+    return navigator.clipboard.writeText(text);
+}
 document.addEventListener('click', function(e){
     const type = e.target.dataset.type;
     if(type == 'lock'){
         const node = e.target.tagName.toLowerCase() == 'i' ? e.target : e.target.children[0];
         node.classList.toggle('fa-lock-open');
         node.classList.toggle('fa-lock');
+    } else if(type == 'copy'){
+        copyText(e.target.textContent);
     }
     
 });
